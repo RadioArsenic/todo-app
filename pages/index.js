7af81fe42ea4ff6930146817e05318954566f22a
Amy Neve <ladyames20203@gmail.com>
2022-11-24 17:10:32 +0800

    now able to create, complete, and delete tasks

--- pages/index.js
+++ pages/index.js
@@ -1,32 +1,74 @@
 import Link from "next/link";
+import { useReducer, useState } from "react";
+import { nanoid } from "nanoid";
+import { ImBin } from 'react-icons/im';
 
 export default function Index() {
-    const items = [
-        {id: 1, action: 'Carve the pumpkin'},
-        {id: 2, action: 'Buy Candy'},
-        {id: 3, action: 'Brainstorm costume ideas'}
+    const OriginalItems = [
+        {id: `${nanoid()}`, action: 'Carve the pumpkin', isComplete: false},
+        {id: `${nanoid()}`, action: 'Buy Candy', isComplete: false},
+        {id: `${nanoid()}`, action: 'Brainstorm costume ideas', isComplete: false}
     ];
 
     function onSubmit(event) {
         event.preventDefault();
-        let todo = document.createElement("li");
-        todo.innerHTML = document.getElementById("NewToDo").value;
-        document.getElementById("ToDo").appendChild(todo);
+        //add to object array
+        let todoAction = document.getElementById("NewToDo").value;
+        let newId = nanoid();
+        items.push({id: newId, action: todoAction, isComplete: false});
+        dispatch({ type: "ADD", id: newId });
     }
     
-    function removeSelf(event) {
-        event.target.parentNode.removeChild(event.target);
+    //TODO: complete function should move item to completed page
+    function reducer(state, action) {
+        switch (action.type) {
+        case 'ADD':
+            return state.map((item) => {
+                return item;
+            });
+        case 'COMPLETE':
+            return state.map((item) => {
+                if (item.id === action.id) {
+                    return { ...item, isComplete: !item.isComplete };
+                }
+                else {
+                    return item;
+                }
+            });
+        case 'DELETE':
+            return state.filter((item) => item.id !== action.id);
+        default:
+            return state;
+        }
+    }
+
+    const [items, dispatch] = useReducer(reducer, OriginalItems);
+
+    function Completed(item) {
+        dispatch({ type: "COMPLETE", id: item.id });
+    }
+
+    function Deleted(item) {
+        dispatch({ type: "DELETE", id: item.id });
     }
 
     return (
     <div>
         <h1>Todo</h1>
-        <ul id="ToDo" onClick={(event) => removeSelf(event)}>
-            {items.map((item) => <li id={item.id}>{item.action}</li>)}
-        </ul>
+
+        {items.map((item) => (
+            <div key={item.id}>
+                <label>
+                    <input type="checkbox" checked={item.isComplete} onChange={() => Completed(item)} />
+                    {item.action}
+                </label>
+                <button onClick={() => Deleted(item)}><ImBin /></button>
+            </div>
+        ))}
+
         Add items using the form below, remove items by clicking on them.
         <form onSubmit={onSubmit}>
-            <label for="NewToDo">Enter the new todo here:</label>
+            <label htmlFor="NewToDo">Enter the new todo here:</label>
             <br />
             <input type="text" id="NewToDo" name="NewToDo"></input>
             <input type="submit" value="submit"></input>
